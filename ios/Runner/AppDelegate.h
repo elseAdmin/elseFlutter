9c6f534c4bef8e9622dc73598d0c3e84736514e7
elseAdmin <needforelse@gmail.com>
2019-11-19 03:45:08 +0530

    1. IOS beacon scan tested on real device 2. calling startRangingBeacons instead of startMonitoringBeacon

--- ios/Runner/AppDelegate.h
+++ ios/Runner/AppDelegate.h
@@ -1,12 +1,9 @@
 #import <Flutter/Flutter.h>
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
-#import <CoreBluetooth/CoreBluetooth.h>
-@interface AppDelegate : FlutterAppDelegate<CLLocationManagerDelegate,CBPeripheralManagerDelegate,CBCentralManagerDelegate>
+@interface AppDelegate : FlutterAppDelegate<CLLocationManagerDelegate>
 
 @property (strong, nonatomic) CLBeaconRegion *beaconRegion;
 @property (strong, nonatomic) CLLocationManager *locationManager;
-@property (strong, nonatomic) CBPeripheralManager *peripheralManager;
-@property (strong, nonatomic) CBCentralManager *centralManager;
 
 @end
--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,46 +1,25 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
-#define DEVICE_INFO_SERVICE_UUID @"00000000-0000-0000-0000-000000000000"
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [GeneratedPluginRegistrant registerWithRegistry:self];
-    
-    self.locationManager = [[CLLocationManager alloc] init];
-    self.locationManager.delegate = self;
-    self.centralManager = [[CBCentralManager alloc] init];
-    self.peripheralManager = [[CBPeripheralManager alloc] init];
-    
-    NSLog(@"peripheral manager and central manager initialized");
-    
-    if (self.peripheralManager.state == CBPeripheralManagerStatePoweredOn)
-    {
-        NSLog(@"peripheral Powered On");
-    }
-    else if (self.peripheralManager.state == CBPeripheralManagerStatePoweredOff)
-    {
-        NSLog(@"peripheral Powered Off");
-    }
-    NSLog(@"fetching authorization status code");
-    CBPeripheralManagerAuthorizationStatus status = [CBPeripheralManager authorizationStatus];
-    NSLog(@"%@",status);
-    //permission
+    //beacon scan code
     if ([CLLocationManager authorizationStatus] != kCLAuthorizationStatusAuthorizedAlways) {
-         NSLog(@"location permission not granted");
+        NSLog(@"location permission not granted");
         [self.locationManager requestAlwaysAuthorization];
         [self.locationManager requestWhenInUseAuthorization];
     } else {
         NSLog(@"location permission already granted");
     }
-    if([CLLocationManager isMonitoringAvailableForClass:self.beaconRegion]){
-        NSLog(@"device found capable of monitoring beacons");
-    }
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
     [self initRegion];
-    [self.locationManager startUpdatingLocation];
-    NSArray *services = [NSArray arrayWithObjects:[CBUUID UUIDWithString:DEVICE_INFO_SERVICE_UUID], nil];
-   
-   FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
+    [self.locationManager startRangingBeaconsInRegion:self.beaconRegion];
+    
+    //flutter native bridge code
+    FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
     FlutterMethodChannel* invokeDartMethod = [FlutterMethodChannel
                                               methodChannelWithName:@"com.else.apis.from.native"
                                               binaryMessenger:controller];
@@ -59,60 +38,18 @@
             NSLog(@"Native Method invoked by flutter");
         }
     }];
-   return [super application:application didFinishLaunchingWithOptions:launchOptions];;
-    // should/can initRegion be called before didFinishLaunchingWithOptions ????
-}
-- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
-{
-    NSLog(@"inside didUpdateLocations");
-    NSLog(@"%@", [locations lastObject]);
+   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
-- (void)centralManagerDidUpdateState:(CBCentralManager *)central {
-    
-    NSLog(@"Check BT status");
-    
-    switch (central.state) {
-        case CBCentralManagerStatePoweredOff:
-            NSLog(@"CoreBluetooth BLE hardware is powered off");
-            break;
-        case CBCentralManagerStateResetting:
-            NSLog(@"CoreBluetooth BLE hardware is resetting");
-            break;
-        case CBCentralManagerStateUnauthorized:
-            NSLog(@"CoreBluetooth BLE state is unauthorized");
-            break;
-        case CBCentralManagerStateUnknown:
-            NSLog(@"CoreBluetooth BLE state is unknown");
-            break;
-        case CBCentralManagerStateUnsupported:
-            NSLog(@"CoreBluetooth BLE hardware is unsupported on this platform");
-            break;
-        case CBCentralManagerStatePoweredOn:
-            NSLog(@"CoreBluetooth BLE hardware is powered on and ready");
-            NSArray *services = [NSArray arrayWithObjects:[CBUUID UUIDWithString:DEVICE_INFO_SERVICE_UUID], nil];
-            [self.centralManager scanForPeripheralsWithServices:services options:nil];
-    }
-    
-}
-
-- (void)peripheralManager:(CBPeripheralManager *)perimanager peripheralManagerDidUpdateState:(CBPeripheralManager *)peripheral{
-    NSLog(@"%@",peripheral.state);
-}
 - (void)initRegion
 {
     static NSString *const kBeaconIdentifier = @"00000000-0000-0000-0000-000000000000";
     static NSString *const kCompanyIdentifier = @"00000000-0000-0000-0000-000000000000";//@"com.ustwo.myRegion";
 
     NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:kBeaconIdentifier];
-    
     // Setup the beacon region we want to look for. Comprises all beacons in that region
     self.beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:uuid
                                                            identifier:kCompanyIdentifier];
-    
-    // Start looking for beacon regions
-    NSLog(@"Monitoring is starting now");
-    [self.locationManager startMonitoringForRegion:self.beaconRegion];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region
@@ -132,8 +69,7 @@
 {
     NSLog(@"inside didRangeBeacons method");
     for(int i = 0; i < [beacons count]; i++){
-        CLBeacon *beacon = [[CLBeacon alloc] init];
-        beacon = [beacons objectAtIndex:i];
+        CLBeacon *beacon = [beacons objectAtIndex:i];
     
     // Update UI
     NSLog(@"%@", beacon.proximityUUID.UUIDString);
