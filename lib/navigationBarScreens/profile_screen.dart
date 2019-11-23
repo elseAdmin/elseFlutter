c6796e9bc6b2178db5671e788267b92edb09c354
rishabh srivastav <srivastav.rishabh@gmail.com>
2019-11-23 19:19:26 +0530

    Profile and About US added

--- lib/navigationBarScreens/profile_screen.dart
+++ lib/navigationBarScreens/profile_screen.dart
@@ -2,13 +2,49 @@
 import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
+
+  final List<String> entries = <String>['Help', 'About', 'Share', 'Rate', 'Logout'];
+
   @override
   Widget build(BuildContext context) {
-    return  ListView(
-        padding: const EdgeInsets.all(8),
-        children: <Widget>[
-          Text('profile page')
-        ],
-      );
+    return ListView.builder(
+      padding: const EdgeInsets.all(8),
+      itemCount: entries.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Center(child: Text('${entries[index]}')),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AboutUsDetailScreen(index: index),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class AboutUsDetailScreen extends StatelessWidget {
+  // Declare a field that holds the Todo.
+  final int index;
+
+  // In the constructor, require a Todo.
+  AboutUsDetailScreen({Key key, @required this.index}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Use the Todo to create the UI.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('About US'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text('Kuch bhi likh diya hai bus'),
+      ),
+    );
   }
 }
