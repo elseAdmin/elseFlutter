88d6f04e7fac32b61c4cc50a55154a84c5d524b5
elseAdmin <needforelse@gmail.com>
2019-12-13 02:02:24 +0530

    1. my events completed 2. flutter upgraded to 1.12 3. bug fixes

--- lib/models/firestore/loc_submission_model.dart
+++ lib/models/firestore/loc_submission_model.dart
@@ -1,12 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:else_app_two/models/base_model.dart';
 
-class LocationEventSubmissionModel {
-  int timestamp;
+class LocationEventSubmissionModel extends BaseModel {
+  DateTime participatedAt;
   Timestamp date;
   String status;
   LocationEventSubmissionModel(DocumentSnapshot snapshot){
     if(snapshot!=null) {
-      this.timestamp = snapshot.data['participatedAt'];
+      this.participatedAt = DateTime.fromMillisecondsSinceEpoch(snapshot.data['participatedAt']);
       this.date = snapshot.data['date'];
       this.status = snapshot.data['status'];
     }
